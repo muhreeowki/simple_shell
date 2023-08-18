f7f597c00da327a6833dc7f2fb7d1eba00fb631c
Kevosa <yegokelvin00@gmail.com>
2023-08-18 16:24:48 +0000

    fixing changing directory

--- builtins.c
+++ builtins.c
@@ -1,77 +1,39 @@
 #include "shell.h"
-/*
 
-
-*/
-int _cd(char **args)
+int _cd(char **args) 
 {
-	char *cwd = NULL;
+    if (args[1] == NULL) 
+    {
+        if (chdir(_getenv("HOME")) == -1) 
+	{
+            perror("chdir");
+            return (-1);
+        }
+    } 
+    else if (_strcmp(args[1], "-") == 0) 
+    {
+        if (chdir(_getenv("OLDPWD")) == -1) 
+	{
+            perror("chdir");
+            return (-1);
+        }
+    } 
+    else 
+    {
+        if (chdir(args[1]) == -1) 
+	{
+            perror("chdir");
+            return (-1);
+        }
+    }
 
-	DIR *directory;
-	struct dirent *dirstruct;
-	if (getcwd(cwd, 0) == NULL)
-	{
-		perror("getcwd");
-		return (-1);
-	}
-	printf("%s",cwd);
-	if (access(cwd, X_OK) == -1)
-	{
-		perror("access");
-		return (-1);
-	}
-	if (args[1] == NULL)
-	{
-		directory = opendir(_getenv("HOME"));
-	}
-	else if (_strcmp(args[1],"-") == 0)
-	{
-		directory = opendir(_getenv("OLDPWD"));
-	}
-	else
-	{
-		directory = opendir(args[1]);
-	}	
-	if (directory == NULL)
-	{
-		perror("opendir");
-		return (-1);
-	}
-	dirstruct = readdir(directory);
-	chdir(dirstruct->d_name);
-/*	closedir(cwd);*/
-	return (0);
+    return (0);
 }
-	
-	
-
-
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 /*
 
-   
    
 
 int _setenv(char **args)
