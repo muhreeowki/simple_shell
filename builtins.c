3e4aced82d27d5ff85c21230eb2cc2039f620dd2
Kevosa <yegokelvin00@gmail.com>
2023-08-18 17:14:50 +0000

    Fixing cd

--- builtins.c
+++ builtins.c
@@ -2,32 +2,48 @@
 
 int _cd(char **args) 
 {
-    if (args[1] == NULL) 
-    {
-        if (chdir(_getenv("HOME")) == -1) 
+	char *cwd;
+	cwd = getcwd(NULL, 0);
+	if (cwd == NULL)
 	{
-            perror("chdir");
-            return (-1);
-        }
-    } 
-    else if (_strcmp(args[1], "-") == 0) 
-    {
-        if (chdir(_getenv("OLDPWD")) == -1) 
+		perror("getcwd");
+		return (-1);
+	}
+	if (args[1] == NULL) 
 	{
-            perror("chdir");
-            return (-1);
-        }
-    } 
-    else 
-    {
-        if (chdir(args[1]) == -1) 
+		if (chdir(_getenv("HOME")) == -1) 
+		{
+		    perror("chdir");
+		    return (-1);
+		}
+		cwd = getcwd(NULL, 0);
+		setenv("PWD", cwd, 1);
+	} 
+	else if (_strcmp(args[1], "-") == 0) 
 	{
-            perror("chdir");
-            return (-1);
-        }
-    }
+		if (chdir(_getenv("OLDPWD")) == -1) 
+		{
+		    perror("chdir");
+		    return (-1);
+		}
+		cwd = getcwd(NULL, 0);
+		setenv("PWD", cwd, 1);
+	} 
+	else 
+	{
+		if (chdir(args[1]) == -1) 
+		{
+		    perror("chdir");
+		    return (-1);
+		}
+		cwd = getcwd(NULL, 0);
+		setenv("PWD", cwd, 1);
+	}
+	
+	setenv("OLDPWD", cwd, 1);
 
-    return (0);
+
+	return (0);
 }
 
 
@@ -46,6 +62,57 @@ int _setenv(char **args)
 
 */
 
+/*
+#include "shell.h"
+
+
+int _cd(char **args) 
+{
+    char *cwd;
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL) 
+    {
+        perror("getcwd");
+        return (-1);
+    }
+    cwd = getcwd(NULL, 0);
+    if (args[1] == NULL) 
+    {
+        if (chdir(_getenv("HOME")) == -1) 
+	{
+            perror("chdir");
+            return (-1);
+        }
+    } 
+    else if (strcmp(args[1], "-") == 0) 
+    {
+        if (chdir(_getenv("OLDPWD")) == -1) 
+	{
+            perror("chdir");
+            return (-1);
+        }
+    } 
+    else 
+    {
+        if (chdir(args[1]) == -1) 
+	{
+            perror("chdir");
+            return (-1);
+        }
+    }
+
+    setenv("OLDPWD", cwd, 1);
+    if (getcwd(cwd, sizeof(cwd)) != NULL) 
+    {
+        setenv("PWD", cwd, 1);
+    } 
+    else 
+    {
+        perror("getcwd");
+    }
+
+    return (0);
+}
 
 
 
@@ -53,5 +120,4 @@ int _setenv(char **args)
 
 
 
-
-
+*/
