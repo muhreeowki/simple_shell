94dc33005e207af1ac326f4dfccc23716cc0a68e
Muriuki <muriuki@muchiri.com>
2023-08-23 14:01:14 +0000

    Betty Documentation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Simple Shell Project by Muriuki and Kelvin
+# Simple Shell Project by Muriuki and Kelvin
+
+This is a simple UNIX command line interpreter that works similarly to the "sh" program.
+
+To use this program, compile with ``gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o hsh``
+
--- builtins.c
+++ builtins.c
@@ -1,9 +1,19 @@
 #include "shell.h"
+/**
+ * _cd - Changes directory
+ *
+ * @args: list of arguments
+ * @name: name of the program
+ * @count: number of commands ran
+ * @status: last exit status
+ *
+ * Return: 0 (success) 2 (failure)
+ */
 
-int _cd(char **args, char *name, int *count, int *status) 
+int _cd(char **args, char *name, int *count, int *status)
 {
 	DIR *dir;
-	char *oldcwd, *newcwd, *msg, newline = '\n', 
+	char *oldcwd, *newcwd, *msg, newline = '\n',
 	     *errmsg[15] = {NULL, NULL, ": ", "cd: can't cd to ", NULL, "\n", NULL};
 
 	errmsg[0] = name;
@@ -11,16 +21,14 @@ int _cd(char **args, char *name, int *count, int *status)
 	errmsg[4] = args[1];
 	msg = _strcat2(errmsg);
 	free(errmsg[1]);
-
 	oldcwd = getcwd(NULL, 0);
 	if (oldcwd == NULL)
-		return(handle_errors(msg, 2));
-
-	if (args[1]) 
+		return (handle_errors(msg, 2));
+	if (args[1])
 	{
 		if (args[1][0] == '-' && args[1][1] == '\0')
 		{
-			newcwd =_getenv("OLDPWD");
+			newcwd = _getenv("OLDPWD");
 			write(STDOUT_FILENO, newcwd, _strlen(newcwd));
 			write(STDOUT_FILENO, &newline, 1);
 		}
@@ -28,17 +36,15 @@ int _cd(char **args, char *name, int *count, int *status)
 		{
 			dir = opendir(args[1]);
 			if (dir == NULL)
-				return(handle_errors(msg, 2));
+				return (handle_errors(msg, 2));
 			closedir(dir);
 			newcwd = args[1];
 		}
 	}
 	else
 		newcwd = _getenv("HOME");
-
-	if (chdir(newcwd) == -1) 
-	    	return (handle_errors(NULL, 2));
-
+	if (chdir(newcwd) == -1)
+		return (handle_errors(NULL, 2));
 	/*setenv("OLDPWD", oldcwd, 1);*/
 	/*setenv("PWD", newcwd, 1);*/
 	free(oldcwd);
@@ -47,18 +53,25 @@ int _cd(char **args, char *name, int *count, int *status)
 	return (0);
 }
 
-
-
+/**
+ * _setenv - Creates or modifies an evnironment vairable
+ *
+ * @args: list of arguments
+ * @name: name of the program
+ * @count: number of commands ran
+ * @status: last exit status
+ *
+ * Return: 0 (success) 2 (failure)
+ */
 
 int _setenv(char **args, char *name, int *count, int *status)
 {
 	int i;
 	int num_vars = 0;
-	char **new_environ, *new_env, *msg, 
-	     *errmsg[6] = {NULL, NULL, ": ", "Usage: setenv VARIABLE VALUE \n", NULL}, 
+	char **new_environ, *new_env, *msg, *errmsg[6] = {NULL, NULL, ": ",
+		     "Usage: setenv VARIABLE VALUE \n", NULL},
 	     *array[4] = {NULL, "=", NULL, NULL};
 
-	/* Error msg */
 	errmsg[0] = name;
 	errmsg[1] =  _itoa(*count, 10);
 	array[0] = args[1];
@@ -66,39 +79,26 @@ int _setenv(char **args, char *name, int *count, int *status)
 	new_env = _strcat2(array);
 	msg = _strcat2(errmsg);
 	free(errmsg[1]);
-
 	(void)status;
-	/* Handle Invalid User INPUT */
 	if (args[1] == NULL || args[2] == NULL)
 		return (handle_errors(msg, 2));
-
-	/* Check if the variable exists */
 	for (i = 0; environ[i] != NULL; i++)
-	{
 		if (_getenv(args[1]))
 		{
 			environ[i] = new_env;
 			free(msg);
 			return (0);
 		}
-	}
-	
-	/* Count variables in environ */
 	while (environ[num_vars])
 		num_vars++;
-
-	/* Reallocate a new list */
 	new_environ = malloc((sizeof(char *)) * (num_vars + 2));
 	if (new_environ == NULL)
-		return(handle_errors(NULL, 2));
-
+		return (handle_errors(NULL, 2));
 	new_env = malloc(sizeof(char) * (strlen(args[1]) + strlen(args[2]) + 2));
 	if (new_env == NULL)
-		return(handle_errors(NULL, 2));
-
+		return (handle_errors(NULL, 2));
 	for (i = 0; environ[i]; i++)
 		new_environ[i] = environ[i];
-
 	new_environ[i++] = new_env;
 	new_environ[i] = NULL;
 	environ = new_environ;
@@ -106,25 +106,32 @@ int _setenv(char **args, char *name, int *count, int *status)
 	return (0);
 }
 
- 
+/**
+ * _unsetenv - Deletes an evnironment vairable
+ *
+ * @args: list of arguments
+ * @name: name of the program
+ * @count: number of commands ran
+ * @status: last exit status
+ *
+ * Return: 0 (success) 2 (failure)
+ */
 int _unsetenv(char **args, char *name, int *count, int *status)
 {
 	int i, target_index = -1;
-	char *var_val, *whole_var = NULL, *msg, 
-	     *errmsg[6] = {NULL, NULL, ": ", "Usage: unsetenv VARIABLE\n", NULL}, 
+	char *var_val, *whole_var = NULL, *msg,
+	     *errmsg[6] = {NULL, NULL, ": ", "Usage: unsetenv VARIABLE\n", NULL},
 	     *array[6] = {NULL, "=", NULL, NULL, NULL};
 
-	/* Error msg */
 	errmsg[0] = name;
 	errmsg[1] =  _itoa(*count, 10);
 	msg = _strcat2(errmsg);
 	free(errmsg[1]);
 	(void)status;
 
-	if (args[1] == NULL) 
-		return(handle_errors(msg, 2));
+	if (args[1] == NULL)
+		return (handle_errors(msg, 2));
 
-	/* Find the var */
 	var_val = _getenv(args[1]);
 	if (var_val)
 	{
@@ -136,31 +143,37 @@ int _unsetenv(char **args, char *name, int *count, int *status)
 			if (_strcmp(environ[i], whole_var) == 0)
 			{
 				target_index = i;
-				/*free(environ[target_index]);*/
 				break;
 			}
 		}
-
 		if (target_index >= 0)
 		{
 			for (i = target_index ; environ[i] != NULL; i++)
 				environ[i] = environ[i + 1];
-		/*	for (i = target_index + 1; environ[i] != NULL; i++)
-				environ[i - 1] = environ[i];*/
 			free(msg);
 			return (0);
-		} 
+		}
 	}
 
-	return(handle_errors(msg, 2));
+	return (handle_errors(msg, 2));
 }
 
 
-
+/**
+ * _exit2 - exits the shell program
+ *
+ * @args: list of arguments
+ * @name: name of the program
+ * @count: number of commands ran
+ * @status: last exit status
+ *
+ * Return: 0 (success) 2 (failure)
+ */
 int _exit2(char **args, char *name, int *count, int *status)
 {
 	int user_status;
-	char *msg, *errmsg[15] = {NULL, NULL, ": ", "exit: Illegal number: ", NULL, "\n", NULL};
+	char *msg, *errmsg[15] = { NULL, NULL, ": ",
+		"exit: Illegal number: ", NULL, "\n", NULL};
 
 	/* Error msg */
 	errmsg[0] = name;
@@ -173,9 +186,8 @@ int _exit2(char **args, char *name, int *count, int *status)
 	{
 		user_status = _atoi(args[1]);
 		if (user_status <= 0)
-			return(handle_errors(msg, 2));
-		else
-			exit(user_status);
+			return (handle_errors(msg, 2));
+		exit(user_status);
 	}
 	free(msg);
 	exit(*status);
@@ -184,7 +196,12 @@ int _exit2(char **args, char *name, int *count, int *status)
 /**
  * _env - prints all the environmental variables
  *
- * Return: nothing.
+ * @args: list of arguments
+ * @name: name of the program
+ * @count: number of commands ran
+ * @status: last exit status
+ *
+ * Return: 0 (success)
  */
 int _env(char **args, char *name, int *count, int *status)
 {
@@ -199,7 +216,7 @@ int _env(char **args, char *name, int *count, int *status)
 	{
 		string = _strcat(environ[i], "\n");
 		write(STDOUT_FILENO, string, _strlen(string));
-		if(string)
+		if (string)
 			free(string);
 	}
 
